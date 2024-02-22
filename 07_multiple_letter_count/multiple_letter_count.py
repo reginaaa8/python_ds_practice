0fe724992370daa54d4332b8f104d8adb08fd297
Regina Steffen <reginasteffen@live.com>
2024-02-22 13:28:21 -0600

    multiple_letter_count function completed

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,9 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letter_count = {}
+
+    for letter in phrase:
+        letter_count[letter] = letter_count.get(letter, 0) + 1
+    
+    return letter_count
