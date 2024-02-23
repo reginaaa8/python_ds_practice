983a5686263a46946745fa64f77fe5da15786376
Regina Steffen <reginasteffen@live.com>
2024-02-22 20:24:34 -0600

    vowel_count function completed

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,13 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = "aeiou"
+
+    phrase = phrase.lower()
+    result = {}
+
+    for ltr in phrase:
+        if ltr in vowels:
+            result[ltr] = result.get(ltr, 0) + 1
+    
+    return result
