c773fcf1183cf9b594f9d5215d404683a3767db8
Regina Steffen <reginasteffen@live.com>
2024-02-22 20:29:44 -0600

    find_factors function completed

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,7 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    list = [n for n in range(1, num // 2 + 1) if num % n == 0]
+    list.append(num)
+    return list
